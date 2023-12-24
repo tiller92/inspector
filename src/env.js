d76ef691087ec41f47eab83837e858628f993b85
tiller92 <ryantiller6@gmail.com>
2023-12-24 13:30:20 -0500

    no db needed

--- src/env.js
+++ src/env.js
@@ -6,7 +6,7 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
+/*  server: {
     DATABASE_URL: z
       .string()
       .url()
@@ -24,7 +24,7 @@ export const env = createEnv({
    * isn't built with invalid env vars. To expose them to the client, prefix them with
    * `NEXT_PUBLIC_`.
    */
-  client: {
+ /* client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
 
@@ -32,7 +32,7 @@ export const env = createEnv({
    * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
    * middlewares) or client-side so we need to destruct manually.
    */
-  runtimeEnv: {
+  /*runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
